447481a60830aab088039b917793b42e8c231d7a
Nayan Bera <nayanbera9@gmail.com>
2025-10-30 01:40:43 +0530

    fixed talwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProviderLayout from "./layouts/ProviderLayout";
 import Home from "./pages/home/Home";
