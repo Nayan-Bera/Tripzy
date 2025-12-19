f5c55abd700c28d48ee7d85e821a467ce2604f6c
Nayan Bera <nayanbera9@gmail.com>
2025-12-19 23:32:11 +0530

    fix

--- src/pages/provider/Providerlayout.tsx
+++ src/pages/provider/Providerlayout.tsx
@@ -3,27 +3,26 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { SiteHeader } from "./_components/site-header";
 import { AppSidebar } from "./_components/app-sidebar";
 
-
 interface AdminLayoutProps {
   children: React.ReactNode;
 }
 
 const ProviderLayout: React.FC<AdminLayoutProps> = ({ children }) => {
   return (
-   <SidebarProvider
-			style={
-				{
-					"--sidebar-width": "calc(var(--spacing) * 72)",
-					"--header-height": "calc(var(--spacing) * 12)",
-				} as React.CSSProperties
-			}
-		>
-			<AppSidebar variant="inset" />
-			<SidebarInset>
-				<SiteHeader />
-				<div className="w-full w-max-6xl p-5">{children}</div>
-			</SidebarInset>
-		</SidebarProvider>
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <div className="w-full w-max-6xl p-5">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 };
 
